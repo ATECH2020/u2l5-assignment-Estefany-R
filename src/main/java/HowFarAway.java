137256107a813913b38473072afa5f670489d1c2
Estefany Rodriguez <estefany.233763@nv.ccsd.net>
2020-10-04 18:48:46 +0000

    Completed HowFarAway

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,22 +4,28 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        double startinglat
-        double startinglong
-        double endinglat
-        double endinglong
-        Scanner scanner = new Scanner(System.in)
+        double startinglat;
+        double startinglong;
+        double endinglat;
+        double endinglong;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the latitude of the starting location: ")
+        System.out.print("Enter the latitude of the starting location: ");
         startinglat = scanner.nextDouble();
-        System.out.println("Enter the longitude of the starting location: ") 
+        System.out.print("Enter the longitude of the starting location: "); 
         startinglong = scanner.nextDouble();
-        System.out.println("Enter the latitude of the ending location: ")
+        System.out.print("Enter the latitude of the ending location: ");
         endinglat = scanner.nextDouble();
-        System.out.println("Enter the longitude of the ending location: ")
+        System.out.print("Enter the longitude of the ending location: ");
         endinglong = scanner.nextDouble();
 
-        
+        GeoLocation locStart = new GeoLocation(startinglat, startinglong);
+
+        GeoLocation locEnd = new GeoLocation(endinglat, endinglong);
+        double distance = locStart.distanceFrom(locEnd);
+        System.out.println("The distance is " + distance +" miles");
+
+
         
     }
 }
