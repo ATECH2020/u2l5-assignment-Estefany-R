6c14e4f92b8700d4bb8189893c1d0689da7f0f03
Estefany Rodriguez <estefany.233763@nv.ccsd.net>
2020-10-04 18:34:13 +0000

    worked on code howfaraway

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -8,5 +8,18 @@ public class HowFarAway
         double startinglong
         double endinglat
         double endinglong
+        Scanner scanner = new Scanner(System.in)
+
+        System.out.println("Enter the latitude of the starting location: ")
+        startinglat = scanner.nextDouble();
+        System.out.println("Enter the longitude of the starting location: ") 
+        startinglong = scanner.nextDouble();
+        System.out.println("Enter the latitude of the ending location: ")
+        endinglat = scanner.nextDouble();
+        System.out.println("Enter the longitude of the ending location: ")
+        endinglong = scanner.nextDouble();
+
+        
+        
     }
 }
